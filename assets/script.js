543b38206b511ee01419c142beddf45009576302
GelP1987 <101074657+GelP1987@users.noreply.github.com>
2022-05-01 23:11:04 -0400

    began seeting up pseudocode in starter code and created issue on github

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,11 @@
 // Assignment code here
-
+// User clicks button to generate password
+// windowprompt presents prompt for password criteria selection
+// User input is used to set choices for characters used, first length 8-128,
+//  then selection of uppercase lowercase and special chatacters
+// then validate user selection and ensure at least one type of character is selected
+// gen password based on user criteria
+// display or window prompt new password
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
